56f5e724f5e0cc6edf331bc8c1f6ae392ebf6a83
casal033 <casal033@morris.umn.edu>
2016-02-14 18:22:44 -0600

    HillClimber is working, random restarts is not...

--- src/simple_search/core_hillclimbing.clj
+++ src/simple_search/core_hillclimbing.clj
@@ -3,13 +3,6 @@
         simple-search.knapsack-examples.knapPI_13_20_1000
         simple-search.knapsack-examples.knapPI_16_20_1000))
 
-;;; An answer will be a map with (at least) four entries:
-;;;   * :instance
-;;;   * :choices - a vector of 0's and 1's indicating whether
-;;;        the corresponding item should be included
-;;;   * :total-weight - the weight of the chosen items
-;;;   * :total-value - the value of the chosen items
-
 (defn included-items [items choices]
   "Takes a sequences of items and a sequence of choices and
   returns the subsequence of items corresponding to the 1's
@@ -29,21 +22,35 @@
      :total-weight (reduce + (map :weight new-instance))
      :total-value (reduce + (map :value new-instance))}))
 
-;;; It might be cool to write a function that
-;;; generates weighted proportions of 0's and 1's.
-
 (defn score [answer]
   "Takes the total-weight of the given answer unless it's over capacity,
   in which case we return 0."
   (if (> (:total-weight answer)
          (:capacity (:instance answer)))
-    0
+    (- (:total-weight answer))
     (:total-value answer)))
 
 (defn add-score [answer]
   "Computes the score of an answer and inserts a new :score field
   to the given answer, returning the augmented answer."
   (assoc answer :score (score answer)))
+
+;;; An answer will be a map with (at least) four entries:
+;;;   * :instance
+;;;   * :choices - a vector of 0's and 1's indicating whether
+;;;        the corresponding item should be included
+;;;   * :total-weight - the weight of the chosen items
+;;;   * :total-value - the value of the chosen items
+
+(defn random-search [instance max-tries]
+  (apply max-key :score
+         (map add-score
+              (repeatedly max-tries #(random-answer instance)))))
+
+;;(time (random-search knapPI_16_20_1000_1 tweaker 100000))
+
+
+;;;;; Our Code starts here ;;;;;
 
 (defn make-instance [instance choices]
   (let [included (included-items (:items instance) choices)]
@@ -52,21 +59,32 @@
      :total-weight (reduce + (map :weight included))
      :total-value (reduce + (map :value included))}))
 
+;; Gets indicies from :choices with the value '0'
 (defn item-index [choices]
-  (map #(- % 1) (filter #(> % 0) (map * (take (count choices) (iterate inc 1)) choices))))
+  (map #(- % 1) (filter #(> % 0) (map * (range 1 (inc (count choices))) choices))))
 
+;; Gets indicies from :choices with the value '1'
 (defn null-item-index [choices]
-  (map #(- % 1) (filter #(> % 0) (remove (set (item-index choices)) (map * (take (count choices) (iterate inc 1)) choices)))))
+  (map #(- % 1) (filter #(> % 0) (remove (set (item-index choices)) (set (take (count choices) (iterate inc 0)))))))
 
+;; Flips a 0 to 1 in :choices
 (defn add-item [instance]
-  (let [index (rand-int (count (item-index (:choices instance))))]
-    (if (== (nth (:choices instance) index) 0)
-      (assoc (:choices instance) index 1))))
+  (let [index (rand-int (count (null-item-index (:choices instance))))]
+    (assoc (:choices instance) index 1)))
 
+;; Flips a 1 to 0 in :choices
 (defn remove-item [instance]
-  (let [index (rand-int (count (item-index (:choices instance))))]
-    (if (== (nth (:choices instance) index) 1)
-      (assoc (:choices instance) index 0))))
+  (let [index (rand-nth (item-index (:choices instance)))]
+    (assoc (:choices instance) index 0)))
+
+;; Simply enough, adds an item if underweight and removes an item if overweight
+(defn tweaker [instance]
+  (println "do we get here?")
+  (make-instance (:instance instance)
+                 ; If there is room in the sac, add something, else remove.
+                 (if (> (:score instance) 0)
+                   (add-item instance)
+                   (remove-item instance))))
 
 (defn rate-it [tweaked-instance current]
   (/ (- (:total-weight tweaked-instance) (:total-weight current)) (:capacity(:instance current))))
@@ -77,14 +95,6 @@
      (<= rate (/ 1 4)) 1
      (and (> rate (/ 1 4)) (< rate 1)) 2
      :else 3 )))
-
-(defn tweaker [instance]
-  (make-instance (:instance instance)
-                 ;; (let [tweaked-choices
-                 ; If there is room in the sac, add something, else remove.
-                 (if (> (:score instance) 0)
-                   (add-item instance)
-                   (remove-item instance))));;])))
 
 (defn tweaker-with-rates [instance]
   (def initial (tweaker instance))
@@ -99,53 +109,38 @@
       current
       (recur current (inc rates) tweak-num mutate))))
 
-(add-score(tweaker (random-search knapPI_16_20_1000_1 1000)))
-(random-search knapPI_16_20_1000_1 10000)
-
-(:score (random-answer knapPI_16_20_1000_1))
-(random-answer knapPI_16_20_1000_1)
-
 (defn hill-search-with-random-restart
-  [instance max-tries]
-  (def start-instance (random-search instance max-tries))
+  [instance mutate-function max-tries]
+  (def start-instance (add-score (random-answer instance)))
+  (println "do we get here?")
   (loop [current start-instance
          last-best start-instance
          tries 0
          counter 0]
-    (let [tweaked-instance (add-score (tweaker-with-rates current))]
+    (let [tweaked-instance (add-score (mutate-function current))]
       (if (> tries max-tries)
         last-best
         (if (> counter 19)
           (if (> (:score current) (:score last-best))
-            (recur (random-search instance 1) current (inc tries) 0)
+            (do ( println (:score current)) (recur (random-search instance 1) current (inc tries) 0))
             (recur (random-search instance 1) last-best (inc tries) 0))
           (if (> (:score tweaked-instance) (:score current))
             (recur tweaked-instance last-best (inc tries) counter)
             (recur current last-best (inc tries) (inc counter))))))))
 
-(time (hill-search-with-random-restart knapPI_16_20_1000_1 100000))
-
-;; Hill Search
+;;(time (hill-search-with-random-restart tweaker knapPI_11_20_1000_1 100000))
 
 (defn hill-search
-  [instance max-tries]
-  (loop [current (random-search instance max-tries)
+  [instance mutate-function max-tries]
+  (def start-instance (add-score (random-answer instance)))
+  (println start-instance)
+  (loop [current start-instance
          tries 0]
-    (let [tweaked-instance (add-score (tweaker current))]
+    (let [tweaked-instance (add-score (mutate-function current))]
       (if (> tries max-tries)
         current
-        (if (> (:score tweaked-instance)
-               (:score current))
-          (recur tweaked-instance (inc tries))
+        (if (> (:score tweaked-instance) (:score current))
+          (do (println (:score tweaked-instance)) (recur tweaked-instance (inc tries)))
           (recur current (inc tries)))))))
 
-(time (hill-search knapPI_16_20_1000_1 100000))
-
-;;; Initial random-search
-
-(defn random-search [instance max-tries]
-  (apply max-key :score
-         (map add-score
-              (repeatedly max-tries #(random-answer instance)))))
-
-(time (random-search knapPI_16_20_1000_1 100000))
+;;;(time (hill-search knapPI_16_20_1000_1 tweaker 1000))
